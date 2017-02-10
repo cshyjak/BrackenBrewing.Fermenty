e99ec99c3b21489848d4da7a90e7277a54cc648c
Craig Shyjak <craig@dolphinidesigns.com>
2017-02-09 21:19:33 -0600

    Sensor setup and screen output

--- BrackenBrewing.Fermenty/BrackenBrewing.Fermenty.ino
+++ BrackenBrewing.Fermenty/BrackenBrewing.Fermenty.ino
@@ -1,121 +1,58 @@
 #include <SoftwareSerial.h>
 #include <OneWire.h>
+#include <DallasTemperature.h>
 
 // Attach the serial display's RX line to digital pin 2
-SoftwareSerial _display(3,2);
+SoftwareSerial _display(3, 2);
+OneWire _oneWire(10);  // on pin 10
+DallasTemperature _sensors(&_oneWire);
+
+const int _relayPin = 3;
+unsigned long _relayStartTime = 0;
+bool _relayState = false;
+const int _minimumRelayToggleTime = 15000;
+double _desiredTemperature = 18.0;
+char _msgBuffer[20];
 
 void setup() {
   Serial.begin(9600);
 
   _display.begin(9600);
   delay(500); // wait for display to boot up
+  
+  _sensors.begin();
+  
+  pinMode(_relayPin, OUTPUT);
 
   _display.write(254); // cursor to beginning of first line
   _display.write(128);
 
-  _display.write("SET TEMP:       "); // clear display + legends
-  _display.write("CUR TEMP:       ");
+  _display.write("DT:      IT:    "); // clear display + legends
+  _display.write("CT:      OT:    ");
 }
 
 void loop() {
-  byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-  float celsius;
+  _sensors.requestTemperatures();
   
-  if ( !ds.search(addr)) {
-    Serial.println("No more addresses.");
-    Serial.println();
-    ds.reset_search();
-    delay(250);
-    return;
-  }
+  WriteToScreen(1, 4, dtostrf(_desiredTemperature, 4, 1, _msgBuffer));
+  WriteToScreen(2, 4, dtostrf(_sensors.getTempCByIndex(0), 4, 1, _msgBuffer));
+  WriteToScreen(1, 13, dtostrf(_sensors.getTempCByIndex(1), 4, 1, _msgBuffer));
+  WriteToScreen(2, 13, dtostrf(_sensors.getTempCByIndex(2), 4, 1, _msgBuffer));
   
-  Serial.print("ROM =");
-  for( i = 0; i < 8; i++) {
-    Serial.write(' ');
-    Serial.print(addr[i], HEX);
+  if(_sensors.getTempCByIndex(0) < _desiredTemperature)
+  {
+    ToggleRelay(false);
   }
-
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return;
+  else
+  {
+    ToggleRelay(true);
   }
-  Serial.println();
- 
-  // the first ROM byte indicates which chip
-  switch (addr[0]) {
-    case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return;
-  } 
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
-  
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-  
-  present = ds.reset();
-  ds.select(addr);    
-  ds.write(0xBE);         // Read Scratchpad
-
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.print(" CRC=");
-  Serial.print(OneWire::crc8(data, 8), HEX);
-  Serial.println();
-
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
-  celsius = (float)raw / 16.0;
-  Serial.print("  Temperature = ");
-  Serial.print(celsius);
-  Serial.print(" Celsius, ");
 }
 
-void WriteToScreen(int line, int startPosition, string text){
+void WriteToScreen(int line, int startPosition, const char* text) {
   int startCharacter = 128;
 
-  if(line == 2){
+  if (line == 2) {
     startCharacter = 192;
   }
 
@@ -124,5 +61,26 @@ void WriteToScreen(int line, int startPosition, string text){
   _display.write(254);
   _display.write(startCharacter);
   _display.write(text);
+}
+
+void ToggleRelay(bool desiredState)
+{
+  if(_relayState != desiredState)
+  {
+    if (_relayState == false)
+    {
+      digitalWrite(_relayPin, HIGH);
+      _relayStartTime = millis();
+      _relayState = true;
+    }
+    else
+    {
+      if((unsigned long)(millis() - _relayStartTime) >= _minimumRelayToggleTime) 
+      {
+        digitalWrite(_relayPin, LOW);
+        _relayState = false;
+      }
+    }
+  }
 }
 
